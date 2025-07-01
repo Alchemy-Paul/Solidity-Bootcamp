707b6173fb9742bfe646bc6a94f51be16ec7dc78
Okoh Paul <63783008+Alchemy-Paul@users.noreply.github.com>
2025-07-01 14:32:51 +0100

    Create basic.sol

---
+++ basic.sol
@@ -0,0 +1,3 @@
+/* Here we use the keywords pragma solidity to specify the version of the Solidity compiler to be used to compile the code. */
+pragma solidity ^0.8.4;
+/* pragma doesn’t change the compiler version, it just requires the compiler to check if the version matches. */
