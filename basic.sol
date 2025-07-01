7af449ba9b169b29d3c3c4ca7b78e5ce51f7cd1b
Okoh Paul <63783008+Alchemy-Paul@users.noreply.github.com>
2025-07-01 14:49:04 +0100

    Update basic.sol
    better way to make comments

--- basic.sol
+++ basic.sol
@@ -1,16 +1,16 @@
-/* Here we use the keywords pragma solidity to specify the version of the Solidity compiler to be used to compile the code. */
+// Here we use the keywords pragma solidity to specify the version of the Solidity compiler to be used to compile the code.
 
 pragma solidity ^0.8.4;
 
-/* pragma doesn’t change the compiler version, it just requires the compiler to check if the version matches. */
-/* n the latest version of ERC20, a compiler version of no less than 0.8.20 is used. 
+// pragma doesn’t change the compiler version, it just requires the compiler to check if the version matches.
+/* the latest version of ERC20, a compiler version of no less than 0.8.20 is used. 
 This way, only compilers with a version greater than or equal to 0.8.20 but less than 0.9.0 can compile this ERC20 contract.
 This helps to avoid potential compatibility issues between the code and compiler versions */
 
 pragma solidity ^0.8.20;
 abstract contract ERC20 is Context, IERC20, IERC20Metadata, IERC20Errors { }
 
-/* We use ^0.a.b to indicate code in the current file needs solidity ≥0.a.b and <0.(a+1).0. */
-/* Here it says we need version >=0.7.5, but <0.8.0 */
+// We use ^0.a.b to indicate code in the current file needs solidity ≥0.a.b and <0.(a+1).0.
+// Here it says we need version >=0.7.5, but <0.8.0
 pragma solidity ^0.7.5;
 
