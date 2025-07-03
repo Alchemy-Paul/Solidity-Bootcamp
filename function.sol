2c2b9e4608837cf38a789b6c102419cdd76f0cf0
Okoh Paul <63783008+Alchemy-Paul@users.noreply.github.com>
2025-07-03 12:47:24 +0100

    example

--- function.sol
+++ function.sol
@@ -8,3 +8,16 @@
 6. [optionally] specific security modifiers */
 //What is a function header?
 // As a black box, we need to define how it can be used, which means its name, inputs, and outputs. This is done by one line of code, we formally call it function header.
+
+//example
+// SPDX-License-Identifier: GPL-3.0 
+pragma solidity ^0.8.4;
+contract Function {
+  //Here we define a function called add, which takes two int, a and b
+  //and outputs another int.
+  //public is indicating that everyone could access this function
+  //we will discuss more about public in lesson Scope
+  function add(int a, int b) public returns(int){
+    return a + b;
+  }
+}
