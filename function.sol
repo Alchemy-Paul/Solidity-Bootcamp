ccdbbca87c173f30038638ab73b72c4ceb2a5aea
Okoh Paul <63783008+Alchemy-Paul@users.noreply.github.com>
2025-07-03 12:38:38 +0100

    definition

--- function.sol
+++ function.sol
@@ -1,1 +1,10 @@
 //a function is a reusable piece of code that represents a specific action or computation within a smart contract
+/* Functions are defined with
+1. a specific name
+2. a list of parameters they take
+3. [optionally] what they return.
+4. different visibility specifiers like public, private, internal, or external, which dictate how the function can be called.
+5. [optionally] view if they don't modify the state or pure if they neither read nor write the state.
+6. [optionally] specific security modifiers */
+//What is a function header?
+// As a black box, we need to define how it can be used, which means its name, inputs, and outputs. This is done by one line of code, we formally call it function header.
